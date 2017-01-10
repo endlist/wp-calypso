59c53c4b235da4639f9356998012d6d9a2dd3393
Kirk Wight <kwight@kwight.ca>
2017-01-10 14:44:21 -0800

    Add a stub page to Site Settings for theme setup (Headstart on Demand).

--- client/my-sites/site-settings/controller.js
+++ client/my-sites/site-settings/controller.js
@@ -19,6 +19,7 @@ import SiteSettingsComponent from 'my-sites/site-settings/main';
 import sitesFactory from 'lib/sites-list';
 import StartOver from './start-over';
 import Taxonomies from './taxonomies';
+import ThemeSetup from './theme-setup';
 import { setDocumentHeadTitle as setTitle } from 'state/document-head/actions';
 import titlecase from 'to-title-case';
 import utils from 'lib/site/utils';
@@ -168,6 +169,13 @@ module.exports = {
 		renderPage(
 			<Taxonomies taxonomy={ context.params.taxonomy } postType="post" />,
 			context
+		);
+	},
+
+	themeSetup( context ) {
+		renderPage(
+			context,
+			<ThemeSetup />
 		);
 	},
 
